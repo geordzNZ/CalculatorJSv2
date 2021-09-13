ffd7cc528e686d19a61fcd8efcabdfd3e2e97471
Geordie King <geordie.king@gmail.com>
2021-09-13 14:16:14 +0100

    added number button listener and linked to display

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+class Calculator {
+  constructor (previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+  }
+
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+  }
+
+  delete() {
+
+  }
+
+  appendNumber(number) {
+    //console.log('b:',number);
+    this.currentOperand = number
+    // console.log('c:',this.currentOperand);
+  }
+
+  chooseOperation(operation) {
+
+  }
+
+  compute() {
+
+  }
+
+  updateDisplay() {
+    // console.log('e:',this.currentOperand);
+    this.currentOperandTextElement.innerText = this.currentOperand
+    // console.log('f:');
+  }
+}
+
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equal]')
@@ -6,3 +46,16 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // console.log('a:',button.innerText);
+    calculator.appendNumber(button.innerText)
+    // console.log('d:');
+    calculator.updateDisplay()
+    // console.log('g:');
+
+  })
+})
