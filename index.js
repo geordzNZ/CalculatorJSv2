778366a2cf9838aaedb1fb9a4920bc4b611f12b5
Geordie King <geordie.king@gmail.com>
2021-09-14 15:08:39 +0100

    add final formatting for commas v2-getDisplayNumber

--- index.js
+++ index.js
@@ -61,15 +61,35 @@ class Calculator {
   getDisplayNumber(number){
     // v1 of formattng the number with commas
     // has issues with decimal places.
-    const floatNumber = parseFloat(number)
-    if (isNaN(floatNumber)) return ''
-    return floatNumber.toLocaleString('en')
+    // const floatNumber = parseFloat(number)
+    // if (isNaN(floatNumber)) return ''
+    // return floatNumber.toLocaleString('en')
+
+    //v2
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+
+    if(isNaN(integerDigits)) {
+      integerDisplay = ''
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+    }
+
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+    } else {
+      return integerDisplay
+    }
   }
 
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
     if (this.operation != null) {
       this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    } else {
+      this.previousOperandTextElement.innerText = ''
     }
   }
 }
