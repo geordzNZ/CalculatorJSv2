142b29ee14769e8d6fffec048fb5a6deeee7fa26
Geordie King <geordie.king@gmail.com>
2021-09-14 15:15:26 +0100

    refactor code a bit

--- index.js
+++ index.js
@@ -16,17 +16,13 @@ class Calculator {
   }
 
   appendNumber(number) {
-    //console.log('b:',number);
-    if (number==='.' && this.currentOperand.includes('.')) return
+    if (number==='.' && this.currentOperand.includes('.')) { return }
     this.currentOperand = this.currentOperand.toString() + number.toString()
-    // console.log('c:',this.currentOperand);
   }
 
   chooseOperation(operation) {
-    if (this.currentOperand === '') return
-    if (this.previousOperand !== '') {
-      this.compute()
-    }
+    if (this.currentOperand === '') { return }
+    if (this.previousOperand !== '') { this.compute() }
     this.operation = operation
     this.previousOperand = this.currentOperand
     this.currentOperand = ''
@@ -36,22 +32,13 @@ class Calculator {
     let computation
     const previous = parseFloat(this.previousOperand)
     const current = parseFloat(this.currentOperand)
-    if (isNaN(previous) || isNaN(current)) return
+    if (isNaN(previous) || isNaN(current)) { return }
     switch (this.operation) {
-      case '+': 
-        computation = previous + current
-        break
-      case '-':
-        computation = previous - current
-        break
-      case '/':
-        computation = previous / current
-        break
-      case '*':
-        computation = previous * current
-        break
-      default:
-        return
+      case '+': computation = previous + current; break;
+      case '-': computation = previous - current; break;
+      case '/': computation = previous / current; break;
+      case '*': computation = previous * current; break;
+      default: return
     }
     this.currentOperand = computation
     this.operation = ''
@@ -59,43 +46,29 @@ class Calculator {
   }
 
   getDisplayNumber(number){
-    // v1 of formattng the number with commas
-    // has issues with decimal places.
-    // const floatNumber = parseFloat(number)
-    // if (isNaN(floatNumber)) return ''
-    // return floatNumber.toLocaleString('en')
-
-    //v2
     const stringNumber = number.toString()
     const integerDigits = parseFloat(stringNumber.split('.')[0])
     const decimalDigits = stringNumber.split('.')[1]
     let integerDisplay
 
-    if(isNaN(integerDigits)) {
-      integerDisplay = ''
-    } else {
-      integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
-    }
+    if(isNaN(integerDigits)) { integerDisplay = '' }
+    else { integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0}) }
 
-    if (decimalDigits != null) {
-      return `${integerDisplay}.${decimalDigits}`
-    } else {
-      return integerDisplay
-    }
+    if (decimalDigits != null) { return `${integerDisplay}.${decimalDigits}` }
+    else { return integerDisplay }
   }
 
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
-    if (this.operation != null) {
-      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
-    } else {
-      this.previousOperandTextElement.innerText = ''
-    }
+    if (this.operation != null) { this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`}
+    else { his.previousOperandTextElement.innerText = '' }
   }
 }
 
+//Create new calculator instance
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
-
+//get calculator items from the web page
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -105,39 +78,35 @@ const previousOperandTextElement = document.querySelector('[data-previous-operan
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
 
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
-
+//Handle number buttons from the web page
 numberButtons.forEach(button => {
   button.addEventListener('click', () => {
-    // console.log('a:',button.innerText);
     calculator.appendNumber(button.innerText)
-    // console.log('d:');
     calculator.updateDisplay()
-    // console.log('g:');
   })
 })
 
+//Handle operation buttons from the web page
 operationButtons.forEach(button => {
   button.addEventListener('click', () => {
-    // console.log('a:',button.innerText);
     calculator.chooseOperation(button.innerText)
-    // console.log('d:');
     calculator.updateDisplay()
-    // console.log('g:');
   })
 })
 
-
+//Handle euqals button from the web page
 equalsButton.addEventListener('click', () => {
   calculator.compute()
   calculator.updateDisplay()
 })
 
+//Handle All Clear button from the web page
 allClearButton.addEventListener('click', () => {
   calculator.clear()
   calculator.updateDisplay()
 })
 
+//Handle delete button from the web page
 deleteButton.addEventListener('click', () => {
   calculator.delete()
   calculator.updateDisplay()
